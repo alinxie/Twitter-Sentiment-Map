8e4ed2a2bd19dc01c351fd2786d96506a82538eb
Victor Chan <victorchan88888888@gmail.com>
2016-11-12 16:32:23 -0800

    python code for tone analysis

---
+++ toneanalysis/tone.py
@@ -0,0 +1,18 @@
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+
+def main():
+
+    tweets = open('input.txt', 'r')
+
+    tone_analyzer = ToneAnalyzerV3(
+        username='fae1b7aa-7534-4fda-b5d1-775076ca668c',
+        password='TbzeEoT4cxHi',
+        version='2016-05-19')
+    
+    tones = open('output.txt', 'w')
+
+    for s in tweets:
+        tones.write(json.dumps(tone_analyzer.tone(s), indent=2))
+
+main()
