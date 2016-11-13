2824e480ae489c88f913d7ade19c7a846f42abe2
Andrew Linxie <alinxie@berkeley.edu>
2016-11-13 01:55:11 +0000

    Switching to Flask ?

---
+++ run.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, request
+import jinja2
+
+app = Flask(__name__, template_folder='static', static_url_path = '/static', static_folder='static')
+PORT = int(os.getenv('PORT', 8000))
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(port = PORT)
