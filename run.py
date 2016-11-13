1ca2192c31117d6d67162f13c292c5fa8a0ea4d1
Andrew Linxie <alinxie@berkeley.edu>
2016-11-13 02:02:18 +0000

    Added cf_deployment_tracker

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, redirect, request
 import jinja2
+import cf_deployment_tracker
+
+cf_deployment_tracker.track()
 
 app = Flask(__name__, template_folder='static', static_url_path = '/static', static_folder='static')
 PORT = int(os.getenv('PORT', 8000))
