13dcf3e3e4dab8696e6b2bca1c4626834ca05f21
eric <eric.kong98@gmail.com>
2016-11-12 22:16:28 -0800

    handling post requests and the twitter feed

--- run.py
+++ run.py
@@ -6,10 +6,20 @@ import cf_deployment_tracker
 cf_deployment_tracker.track()
 
 app = Flask(__name__, template_folder='templates', static_url_path = '/static', static_folder='static')
-PORT = int(os.getenv('PORT', 8000))
-@app.route("/")
+PORT = int(os.getenv('PORT', 5000))
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    # Default value is america
+    subject = "america"
+    name = subject
+    if request.method == 'POST':
+        name = request.form['subject']
+        subject = "https://twitter.com/hashtag/" + request.form['subject']
+        print (subject)
+        print (name)
+        return render_template("index.html", subject=subject, name=name)
+
+    return render_template("index.html", subject=subject, name=name)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = PORT)
